18561b8ba3198dc287951f4f042381f257030580
Wexa <calledhypocrisy@gmail.com>
2023-01-18 17:11:36 +0300

    sstream

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <iostream>
+#include <sstream>
 
 int main( int argc, char** argv ){
 
-	std::cout << "Ready" << std::endl;
+	std::ostringstream s;
+	s << "Hello" << "World";
+
+	std::cout << s.str() << std::endl;
 
 	return 0;
 };
